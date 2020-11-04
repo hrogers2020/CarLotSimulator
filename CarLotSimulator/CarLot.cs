42e063413e7775f861006ab8e96dcfdc98ef621b
Hampton Rogers <rogershampton22@gmail.com>
2020-11-04 12:04:13 -0600

    Silly Simulator: could use some refactoring

---
+++ CarLotSimulator/CarLot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    class CarLot
+    {
+        public List<Car> Lot { get; set; }
+    }
+}
--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -8,10 +9,64 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property            
+            //Create a seperate class file called Car: Check.
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable: Check.
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise(): Check.
+            //The methods should take one string parameter: the respective noise property: Check.
+
+            //Standard Member Initialization
+            Car acura = new Car();
+            acura.Year = 2014;
+            acura.Make = "Acura:";
+            acura.Model = "RDX";
+            acura.EngineNoise = "Engine Note: Deep hum";
+            acura.HonkNoise = "Horn: Robust honk";
+            acura.IsDriveable = true;
+
+            acura.CarMake();
+            acura.MakeEngineNoise();
+            acura.MakeHonkNoise();
+
+            Car honda = new Car();
+            honda.Year = 2010;
+            honda.Make = "Honda:";
+            honda.Model = "Civic";
+            honda.EngineNoise = "Engine Note: Reeeeee + whineeee (terrible)";
+            honda.HonkNoise = "Horn: Dainty lil pip-squeak honk";
+            honda.IsDriveable = true;
+
+            honda.CarMake();
+            honda.MakeEngineNoise();
+            honda.MakeHonkNoise();
+
+            Car ford = new Car();
+            ford.Year = 2003;
+            ford.Make = "Ford:";
+            ford.Model = "Explorer";
+            ford.EngineNoise = "Engine Note: Vroooom, guzzle-gas!";
+            ford.HonkNoise = "Horn: BEEP FREAKIN' BEEP";
+            ford.IsDriveable = false;
+
+            ford.CarMake();
+            ford.MakeEngineNoise();
+            ford.MakeHonkNoise();
+
+            var car1 = new Car();
+            car1.Model = "Acura";
+
+            var car2 = new Car();
+            car2.Model = "Honda";
+
+            var car3 = new Car();
+            car3.Model = "Ford";
+
+            var parking = new CarLot();
+            parking.Lot = new List<Car>() { car1, car2, car3 };
+
+            foreach(var car in parking.Lot)
+            {
+                Console.WriteLine($"{car.Model} {car.Year}");
+            }
 
 
             //Now that the Car class is created we can instanciate 3 new cars
